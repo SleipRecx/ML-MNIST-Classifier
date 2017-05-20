40ab100bf9a57b3c1b274e010f110a574d685278
Markus Andresen <markua@stud.ntnu.no>
2017-05-20 21:10:35 +0200

    1nearest to knearest

--- knn_self_implemented.py
+++ knn_self_implemented.py
@@ -1,40 +1,45 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial import distance
 import time
+import math
+import operator
+import matplotlib.pyplot as plt
 
+from collections import Counter
+from scipy.spatial import distance
 from sklearn.datasets import fetch_mldata
 from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
+from sklearn.utils import resample
 from sklearn.metrics import accuracy_score
 
-class NearestNeighbor():
-    def fit(self, X_train, y_train):
+class KNearestNeighbors():
+    def fit(self, X_train, y_train, k):
         self.X_train = X_train
         self.y_train = y_train
+        self.k = k
 
     def predict(self, X_test):
         predictions = []
         for row in X_test:
-            label = self.closest(row)
+            label = self.k_closest(row)
             predictions.append(label)
         return predictions
 
-    def closest(self, row):
-        best_distance = distance.euclidean(row, self.X_train[0])
-        best_index = 0
-        for i in range(1, len(self.X_train)):
+    def k_closest(self, row):
+        distances = []
+        for i in range(len(self.X_train)):
             current_dist = distance.euclidean(row, self.X_train[i])
-            if current_dist < best_distance:
-                best_distance = current_dist
-                best_index = i
-        return self.y_train[best_index]
+            distances.append((current_dist, self.y_train[i]))
+        distances.sort(key=operator.itemgetter(0))
+        common_list = distances[:self.k]
+        counter = Counter(element[1] for element in common_list)
+        return counter.most_common()[0][0]
+
 
 
 # Import dataset and place it in current dir
 mnist = fetch_mldata('MNIST original', data_home=os.path.dirname(os.path.realpath(__file__)))
-mnist.data, mnist.target = shuffle(mnist.data, mnist.target)
+mnist.data, mnist.target = resample(mnist.data, mnist.target)
 X = mnist.data[:2000]
 y = mnist.target[:2000]
 
@@ -42,12 +47,16 @@ y = mnist.target[:2000]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
 # Create classifier
-classifier = NearestNeighbor()
+classifier = KNearestNeighbors()
+
+
+# Choose k value based on training examples
+k = int(math.sqrt(len(X_train)))
 
 # Train classifier
-print 'Training classifier with', len(X_train), 'training examples'
+print 'Training classifier with', len(X_train), 'training examples and k =', k
 start = time.time()
-classifier.fit(X_train, y_train)
+classifier.fit(X_train, y_train, k)
 print 'Training took', (time.time() - start), 'Seconds'
 
 # Use classifier to predict numbers from images in test set
